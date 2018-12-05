7386505d333366161b650a3356045d80e390859e
Mike. Shi <ashida2016@outlook.com>
2018-12-05 11:00:15 +0800

    updated 14

--- source/14-name.py
+++ source/14-name.py
@@ -4,14 +4,12 @@
 # author by : （学员ID)
 
 # 要点：初步理解元组和列表
-import os
+
 import sys
-import codecs
 import io
 
-#print (family_name[0].encode('utf-8'))
-#sys.stdout = codecs.getwriter('gb2312')(sys.stdout)
-sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='gb2312')
+# 解决输出显示汉字乱码的问题
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf-8')
 print (sys.stdout.encoding)
 
 
